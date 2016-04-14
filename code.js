cdb5331da136778579c59638c33c0e098e694e92
Liann Sun <lianndabomb@yahoo.com>
2016-04-14 21:13:41 +0000

    prettify js

--- code.js
+++ code.js
@@ -1,9 +1,16 @@
 $(".button").on("click", function() {
-var value = encodeURIComponent($("input").val()); // encodeURIComponent lets input with spaces, e.g. "new york", work.
-function makeSrc(center) {return "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=10&size=600x300";
+    var value = encodeURIComponent($("input").val()); // encodeURIComponent lets input with spaces, e.g. "new york", work.
+    function makeSrc(center) {
+        return "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=10&size=600x300";
+    };
+    var link=makeSrc(center);
+
+    var img="<img src="+link+">";
+
+    $(".map").append(img);
+    
+});
+
+function makeSrc(center){
+    return "i am a beautiful " + center + " teapot!";
 };
-var link=makeSrc(center);
-var img="<img src="+link+">";
-(".map").append(img);});
-function makeSrc(center){return "i am a beautiful " + center + " teapot!";};
-console.log("hi");
