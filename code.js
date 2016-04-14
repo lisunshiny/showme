9d6515a0f6ba760b99b37c2e79f4fd775319f2df
Liann Sun <lianndabomb@yahoo.com>
2016-04-14 21:34:36 +0000

    document.ready addition, initialize document

--- code.js
+++ code.js
@@ -1,16 +1,20 @@
-$(".button").on("click", function() {
-    var value = encodeURIComponent($("input").val()); // encodeURIComponent lets input with spaces, e.g. "new york", work.
-    function makeSrc(center) {
-        return "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=10&size=600x300";
-    };
-    var link=makeSrc(center);
-
-    var img="<img src="+link+">";
-
-    $(".map").append(img);
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $(".button").on("click", function() {
+        console.log("hi");
+        var value = encodeURIComponent($("input").val()); // encodeURIComponent lets input with spaces, e.g. "new york", work.
+        function makeSrc(center) {
+            return "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=10&size=600x300";
+        }
+        var link=makeSrc(center);
     
+        var img="<img src="+link+">";
+    
+        $(".map").append(img);
+        
+    });
+    
+    function makeSrc(center){
+        return "i am a beautiful " + center + " teapot!";
+    }
 });
-
-function makeSrc(center){
-    return "i am a beautiful " + center + " teapot!";
-};
